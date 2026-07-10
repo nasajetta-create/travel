60088323b1a050113303453273f77a65e89ac4c1
nasajetta <nasa.jetta@gmail.com>
2026-07-10 12:42:14 +0800

    T0710-09 mobile improvements

--- sw-tsplit.js
+++ sw-tsplit.js
@@ -1,4 +1,4 @@
-var C = 'tsplit-v8';
+var C = 'tsplit-v9';
 self.addEventListener('install', function(e){ self.skipWaiting(); });
 self.addEventListener('activate', function(e){
   e.waitUntil(caches.keys().then(function(ks){
