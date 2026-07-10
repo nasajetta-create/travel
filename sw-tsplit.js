2fa39ddf8965de13756f1fbc78ce5a0194c27345
nasajetta <nasa.jetta@gmail.com>
2026-07-10 13:23:03 +0800

    Add files via upload
    T0710-10 UX round 2

--- sw-tsplit.js
+++ sw-tsplit.js
@@ -1,4 +1,4 @@
-var C = 'tsplit-v9';
+var C = 'tsplit-v10';
 self.addEventListener('install', function(e){ self.skipWaiting(); });
 self.addEventListener('activate', function(e){
   e.waitUntil(caches.keys().then(function(ks){
