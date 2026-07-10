f992ef9949dedb7b92c2d5a15810c86c4fb86ae5
nasajetta <nasa.jetta@gmail.com>
2026-07-10 13:45:30 +0800

    Add files via upload
    T0710-11 polish and member import

--- sw-tsplit.js
+++ sw-tsplit.js
@@ -1,4 +1,4 @@
-var C = 'tsplit-v10';
+var C = 'tsplit-v11';
 self.addEventListener('install', function(e){ self.skipWaiting(); });
 self.addEventListener('activate', function(e){
   e.waitUntil(caches.keys().then(function(ks){
