ea82fe42f48457d0e9afe05f4f0169585e99daed
nasajetta <nasa.jetta@gmail.com>
2026-07-10 21:10:41 +0800

    T0710-19 說明精簡+置頂可取消

--- sw-tsplit.js
+++ sw-tsplit.js
@@ -1,4 +1,4 @@
-var C = 'tsplit-v18';
+var C = 'tsplit-v19';
 self.addEventListener('install', function(e){ self.skipWaiting(); });
 self.addEventListener('activate', function(e){
   e.waitUntil(caches.keys().then(function(ks){
