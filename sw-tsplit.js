9f4627124b25485993e473cc81853821a742c77c
nasajetta <nasa.jetta@gmail.com>
2026-07-11 19:31:40 +0800

    T0711-08

--- sw-tsplit.js
+++ sw-tsplit.js
@@ -1,4 +1,4 @@
-var C = 'tsplit-v26'; /* T0711-07 */
+var C = 'tsplit-v27'; /* T0711-08 */
 self.addEventListener('install', function(e){ self.skipWaiting(); });
 self.addEventListener('activate', function(e){
   e.waitUntil(caches.keys().then(function(ks){
