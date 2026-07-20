68f7d180d1e2325a35de1313f32b8a409fd7f09b
nasajetta <nasa.jetta@gmail.com>
2026-07-20 08:10:58 +0800

    T0720-05 拍照/相簿雙鍵＋收據翻譯

--- sw-tsplit.js
+++ sw-tsplit.js
@@ -1,4 +1,4 @@
-var C = 'tsplit-v62'; /* T0720-04 */
+var C = 'tsplit-v63'; /* T0720-05 */
 self.addEventListener('install', function(e){ self.skipWaiting(); });
 self.addEventListener('activate', function(e){
   e.waitUntil(caches.keys().then(function(ks){
